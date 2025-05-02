a09f8917a040b5d0d246a43b1068e9663090d197
Reza Dianat <dianat.reza66@gmail.com>
2025-05-02 13:56:15 +0200

    implement home page structure and styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,21 @@
+import { Link } from "react-router-dom";
 import styles from "./Home.module.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className={styles.hero}>
+      <div className={styles.heroContent}>
+        <h1 className={styles.heroTitle}>Game Horizon</h1>
+        <p className={styles.heroSubtitle}>
+          Game Horizon—Where Every Adventure Begins. Your Portal to Infinite
+          Gaming Worlds. Play Beyond Limits.
+        </p>
+        <Link to="/games" className={styles.ctaButton}>
+          Explore Games
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
