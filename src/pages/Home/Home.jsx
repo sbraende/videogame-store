e33d51cdcb540870c87f21b2fcd7cad6b2ba6119
Reza Dianat <dianat.reza66@gmail.com>
2025-05-02 15:01:03 +0200

    implement the form structure and styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,16 +4,20 @@ import styles from "./Home.module.css";
 const Home = () => {
   return (
     <section className={styles.hero}>
-      <div className={styles.heroContent}>
+      <header className={styles.heroContent}>
         <h1 className={styles.heroTitle}>Game Horizon</h1>
         <p className={styles.heroSubtitle}>
           Game Horizon—Where Every Adventure Begins. Your Portal to Infinite
           Gaming Worlds. Play Beyond Limits.
         </p>
-        <Link to="/games" className={styles.ctaButton}>
+        <Link
+          to="/games"
+          className={styles.ctaButton}
+          aria-label="Explore available games"
+        >
           Explore Games
         </Link>
-      </div>
+      </header>
     </section>
   );
 };
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,7 +1,88 @@
 import styles from "./SignUp.module.css";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  // Declaring state variables
+  const [signUpFormData, setSignUpFormData] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    dateOfBirth: "",
+    profilePicture: null, // Stores the actual file
+    imageUrl: "", // Stores the preview URL
+  });
+  return (
+    <div className={styles.formWrapper}>
+      <form className={styles.signUpForm}>
+        <h1>Sign-up Form</h1>
+        <fieldset className={styles.formGroup}>
+          <legend className={styles.formGroupTitle}>
+            Personal Information
+          </legend>
+          <label htmlFor="firstname">First name</label>
+          <input
+            type="text"
+            name="firstname"
+            id="firstname"
+            className={styles.formInput}
+          />
+          <p className={styles.errorMessage}>Error</p>
+          <label htmlFor="lastname">Last name</label>
+          <input
+            type="text"
+            name="lastname"
+            id="lastname"
+            className={styles.formInput}
+          />
+          <p className={styles.errorMessage}>Error</p>
+          <label htmlFor="dateOfBirth">Date of Birth</label>
+          <input
+            type="date"
+            name="dateOfBirth"
+            id="dateOfBirth"
+            className={styles.formInput}
+          />
+          <label htmlFor="profilePicture">Profile picture</label>
+          <input
+            type="file"
+            name="profilePicture"
+            id="profilePicture"
+            className={styles.formInput}
+            accept=".jpg, .jpeg, .png"
+          />
+        </fieldset>
+        <fieldset className={styles.formGroup}>
+          <legend className={styles.formGroupTitle}>Account Details</legend>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className={styles.formInput}
+          />
+          <p className={styles.errorMessage}>Error</p>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            className={styles.formInput}
+          />
+          <p className={styles.errorMessage}>Error</p>
+          <label htmlFor="confirmPassword">Confirm password</label>
+          <input
+            type="password"
+            name="confirmPassword"
+            id="confirmPassword"
+            className={styles.formInput}
+          />
+          <p className={styles.errorMessage}>Error</p>
+        </fieldset>
+        <button className={styles.createAccountButton}>Create Account</button>
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
