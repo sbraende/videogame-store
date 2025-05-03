d59cfbce4ff589ca5fc7c184344150e481cb2ce2
Reza Dianat <dianat.reza66@gmail.com>
2025-05-03 12:15:51 +0200

    implement auth context

---
+++ src/context/authContext.js
@@ -0,0 +1,23 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../../firebaseConfig";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+
+    return () => unsubscribe(); // Cleanup on unmount
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import "./index.css";
 import App from "./App.jsx";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/routes.jsx";
+import { AuthProvider } from "./context/authContext.js";
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
-  <RouterProvider router={router}>
-    <App />
-  </RouterProvider>
+  <AuthProvider>
+    <RouterProvider router={router} />
+  </AuthProvider>
+
   // </StrictMode>,
 );
