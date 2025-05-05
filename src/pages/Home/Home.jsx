b0afe5fae32869cf1be5f01e0991bb2ec7069a80
sbraende <sebastian.braende@gmail.com>
2025-05-05 10:16:34 +0200

    add: home-page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,25 @@
+import { Link } from "react-router-dom";
 import styles from "./Home.module.css";
 
 const Home = () => {
-  return <section>Home Page</section>;
+  return (
+    <section className={styles.hero}>
+      <header className={styles.heroContent}>
+        <h1 className={styles.heroTitle}>Game Horizon</h1>
+        <p className={styles.heroSubtitle}>
+          Game Horizon - Where Every Adventrue Begins - Your portal to Infinite
+          Gaming Worlds. Play beyond Limits.
+        </p>
+        <Link
+          to={"/games"}
+          className={styles.ctaButton}
+          aria-label="Explore available games"
+        >
+          Explore Games
+        </Link>
+      </header>
+    </section>
+  );
 };
 
 export default Home;
